d8de1af5e906da21d8ac9c69dd7963d25ba12b44
Edoardo Gargano <cnt000@gmail.com>
2020-03-06 14:23:18 +0100

    feat: cors enabled on *

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const path = require('path')
 require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
 
 fastify.register(require('./products-route'))
 
+fastify.register(require('fastify-cors'), {
+  origin: '*'
+})
+
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000)
+    await fastify.listen(process.env.PORT || 8080)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
