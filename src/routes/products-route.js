1d8089101be15d2f46fa937b0c3d6e15c9b79470
Edoardo Gargano <edoardo.gargano@monrif.net>
2020-09-23 16:50:58 +0200

    search route

--- src/routes/products-route.js
+++ src/routes/products-route.js
@@ -1,5 +1,12 @@
 const { Storage } = require('@google-cloud/storage')
-const storage = new Storage()
+const GOOGLE_CLOUD_PROJECT_ID = 'pungilandia-gcs';
+const GOOGLE_CLOUD_KEYFILE = './pungilandia-gcs-0d178b593aff.json';
+
+const storage = new Storage({
+  projectId: GOOGLE_CLOUD_PROJECT_ID,
+  keyFilename: GOOGLE_CLOUD_KEYFILE,
+});
+
 const bucket = storage.bucket(process.env.BUCKET_NAME)
 const { replyOk, replyNotFound } = require('../replies/reply')
 
@@ -47,6 +54,38 @@ async function routes(fastify) {
         })
         .on('end', () => {
           replyOk(reply, content)
+        })
+    } catch (e) {
+      replyNotFound(reply)
+    }
+  })
+
+  fastify.get('/search', async (request, reply) => {
+    const query = request.query.q;
+    const fileName = `${plpDirectory}${plpPrefix}0.json`
+    const file = bucket.file(fileName)
+    let content = ''
+    const data = await file.get()
+    try {
+      data[0]
+        .createReadStream()
+        .on('error', err => {
+          return Promise.reject(err)
+        })
+        .on('data', data => {
+          content += data
+        })
+        .on('end', () => {
+          const products = JSON.parse(content)
+          const regex = new RegExp(query, 'ig')
+          const filtered = products.filter(({name}) => regex.test(name))
+          reply
+            .code(200)
+            .type('application/json')
+            .header('Content-Type', 'application/json charset=utf-8')
+            .send(filtered)
+          // replyOk(reply, products.toString())
+          // TODO
         })
     } catch (e) {
       replyNotFound(reply)
